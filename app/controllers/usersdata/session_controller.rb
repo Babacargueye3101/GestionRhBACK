cce3a701a0a9327fafc85fbeaa2a358a9cde531e
Babacar Gueye <babacar.gueye@eyone.com>
2024-08-29 16:19:31 +0000

    comment attachement

--- app/controllers/usersdata/session_controller.rb
+++ app/controllers/usersdata/session_controller.rb
@@ -1,14 +1,16 @@
 # app/controllers/users/sessions_controller.rb
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+module Usersdata
+  class SessionController < Devise::SessionsController
+    respond_to :json
 
-  private
+    private
 
-  def respond_with(resource, _opts = {})
-    render json: { message: 'Logged in successfully.', user: resource }, status: :ok
-  end
+    def respond_with(resource, _opts = {})
+      render json: { message: 'Logged in successfully.', user: resource }, status: :ok
+    end
 
-  def respond_to_on_destroy
-    render json: { message: 'Logged out successfully.' }, status: :ok
+    def respond_to_on_destroy
+      render json: { message: 'Logged out successfully.' }, status: :ok
+    end
   end
 end
