6da6cfac479c68ef3c781ffe68cca5eee78f23e3
Babacar Gueye <babacar.gueye@eyone.com>
2024-08-29 16:29:45 +0000

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   namespace :api do
     get 'announcements/index'
     get 'announcements/show'
