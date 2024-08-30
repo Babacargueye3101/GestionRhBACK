004fbfa6e34f271d2df49013860ec8b17d390b38
Babacar Gueye <babacar.gueye@eyone.com>
2024-08-30 08:51:46 +0000

    add migration logo

--- db/migrate/20240811145513_add_attachment_logo_to_compagnies.rb
+++ db/migrate/20240811145513_add_attachment_logo_to_compagnies.rb
@@ -1,11 +1,11 @@
 class AddAttachmentLogoToCompagnies < ActiveRecord::Migration[6.1]
-  # def self.up
-  #   change_table :compagnies do |t|
-  #     t.attachment :logo
-  #   end
-  # end
+  def self.up
+    change_table :compagnies do |t|
+      t.attachment :logo
+    end
+  end
 
-  # def self.down
-  #   remove_attachment :compagnies, :logo
-  # end
+  def self.down
+    remove_attachment :compagnies, :logo
+  end
 end
