b66baf978906fc394dcf5b48ab2a55ad6599bc00
Babacar Gueye <babacar.gueye@eyone.com>
2024-09-02 11:58:06 +0000

    add migration for compagny

--- db/migrate/20240902113147_remove_paperclip_columns_from_companies.rb
+++ db/migrate/20240902113147_remove_paperclip_columns_from_companies.rb
@@ -1,8 +1,19 @@
 class RemovePaperclipColumnsFromCompanies < ActiveRecord::Migration[7.2]
   def change
-    remove_column :compagnies, :logo_file_name, :string
-    remove_column :compagnies, :logo_content_type, :string
-    remove_column :compagnies, :logo_file_size, :integer
-    remove_column :compagnies, :logo_updated_at, :datetime
+    if column_exists?(:compagnies, :logo_file_name)
+      remove_column :compagnies, :logo_file_name
+    end
+
+    if column_exists?(:compagnies, :logo_content_type)
+      remove_column :compagnies, :logo_content_type
+    end
+
+    if column_exists?(:compagnies, :logo_file_size)
+      remove_column :compagnies, :logo_file_size
+    end
+
+    if column_exists?(:compagnies, :logo_updated_at)
+      remove_column :compagnies, :logo_updated_at
+    end
   end
 end
