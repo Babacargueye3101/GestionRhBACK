bc92ee8929e08a9cadc7fe439c96aca1b7e1c034
Babacar Gueye Root <babacargueye3101@gmail.com>
2025-04-20 09:34:01 +0000

    comment thiss line

--- db/migrate/20240816124831_add_compagny_id_to_payment.rb
+++ db/migrate/20240816124831_add_compagny_id_to_payment.rb
@@ -1,5 +1,5 @@
 class AddCompagnyIdToPayment < ActiveRecord::Migration[6.1]
   def change
-    add_column :payments, :compagny_id, :integer
+    #add_column :payments, :compagny_id, :integer
   end
 end
